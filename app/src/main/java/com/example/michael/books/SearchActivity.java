7e2f0d0c547eeac9112886a5c3039fd2571ca936
Michael Hartley <mtjhartley@gmail.com>
2018-05-14 22:48:20 -0700

    finished module, added shared preferences and dynamically created menus

--- app/src/main/java/com/example/michael/books/SearchActivity.java
+++ app/src/main/java/com/example/michael/books/SearchActivity.java
@@ -1,5 +1,6 @@
 package com.example.michael.books;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -34,6 +35,20 @@ public class SearchActivity extends AppCompatActivity {
                 }
                 else {
                     URL queryURL = ApiUtil.buildUrl(title, authors, publisher, isbn);
+
+                    //SharedPreferences
+                    Context context = getApplicationContext();
+                    int position = SpUtil.getPreferenceInt(context, SpUtil.POSITION);
+                    if (position == 0 || position == 5) {
+                        position = 1;
+                    } else{
+                        position++;
+                    }
+                    String key = SpUtil.QUERY + String.valueOf(position);
+                    String value = title + "," + authors + "," + publisher + "," + isbn;
+                    SpUtil.setPreferenceString(context, key, value);
+                    SpUtil.setPreferenceInt(context, SpUtil.POSITION, position);
+
                     Intent intent = new Intent(getApplicationContext(), BookListActivity.class);
                     intent.putExtra("query", queryURL.toString());
                     startActivity(intent);
